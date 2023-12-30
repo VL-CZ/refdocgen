cb9e28a0fe7c28e5adb4cf8e12aad7ecd5c73841
Vojtěch Lengál <vojta.lengal@gmail.com>
2023-12-30 21:17:17 +0100

    project added

--- src/RefDocGen/Program.cs
+++ src/RefDocGen/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace RefDocGen
+﻿namespace RefDocGen;
+
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
