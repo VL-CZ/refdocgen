0bc60b9ad81fafca00c2001d4966f4add0eefd6c
Vojtěch Lengál <vojta.lengal@gmail.com>
2024-06-27 20:57:08 +0200

    added isXX fields to properties

--- src/RefDocGen/AssemblyAnalyzer.cs
+++ src/RefDocGen/AssemblyAnalyzer.cs
@@ -34,16 +34,39 @@ public class AssemblyAnalyzer
         var properties = type.GetProperties(bindingFlags).Where(f => !f.IsCompilerGenerated());
         var methods = type.GetMethods(bindingFlags).Where(f => !f.IsCompilerGenerated());
 
-        var fieldModels = fields.Select(f => new FieldIntermed(f.Name, f.FieldType.Name, f.GetAccessibilityModifier(), f.IsStatic)).ToArray();
-
-        var propertyModels = properties.Select(p => new PropertyIntermed(p.Name, p.PropertyType.Name, p.GetAccessibilityModifier(), p.GetMethod?.GetAccessibilityModifier(), p.SetMethod?.GetAccessibilityModifier())).ToArray();
-
-        var methodModels = methods.Select(m =>
-            new MethodIntermed(m.Name,
-            m.GetParameters().Select(p => new MethodParameter(p.Name, p.ParameterType.Name)).ToArray(),
-            m.ReturnType.Name, AccessibilityModifier.Public, m.IsStatic, m.IsVirtual, m.IsAbstract)
-        ).ToArray();
+        var fieldModels = fields.Select(ConstructField).ToArray();
+        var propertyModels = properties.Select(ConstructProperty).ToArray();
+        var methodModels = methods.Select(ConstructMethod).ToArray();
 
         return new ClassIntermed(type.FullName, AccessibilityModifier.Public, fieldModels, propertyModels, methodModels);
+    }
+
+    private FieldIntermed ConstructField(FieldInfo field)
+    {
+        return new FieldIntermed(field.Name, field.FieldType.Name, field.GetAccessibilityModifier(), field.IsStatic);
+    }
+
+    private PropertyIntermed ConstructProperty(PropertyInfo property)
+    {
+        var accessors = property.GetAccessors();
+
+        bool isStatic = accessors.All(a => a.IsStatic);
+        bool isAbstract = accessors.All(a => a.IsAbstract);
+        bool isVirtual = accessors.All(a => a.IsVirtual);
+
+        return new PropertyIntermed(property.Name, property.PropertyType.Name, property.GetAccessibilityModifier(),
+            property.GetMethod?.GetAccessibilityModifier(), property.SetMethod?.GetAccessibilityModifier(),
+            isStatic, isVirtual, isAbstract);
+    }
+
+    private MethodIntermed ConstructMethod(MethodInfo method)
+    {
+        var parameters = method.GetParameters().Select(ConstructMethodParameter).ToArray();
+        return new MethodIntermed(method.Name, parameters, method.ReturnType.Name, method.GetAccessibilityModifier(), method.IsStatic, method.IsVirtual, method.IsAbstract);
+    }
+
+    private MethodParameter ConstructMethodParameter(ParameterInfo p)
+    {
+        return new MethodParameter(p.Name, p.ParameterType.Name);
     }
 }
