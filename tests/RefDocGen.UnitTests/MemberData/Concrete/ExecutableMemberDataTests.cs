938927cc58394f740eba05a16c5777c2dd001406
Vojtěch Lengál <vojta.lengal@gmail.com>
2024-11-10 23:26:16 +0100

    tests pass

--- tests/RefDocGen.UnitTests/MemberData/Concrete/ExecutableMemberDataTests.cs
+++ tests/RefDocGen.UnitTests/MemberData/Concrete/ExecutableMemberDataTests.cs
@@ -24,19 +24,42 @@ public class ExecutableMemberDataTests
     }
 
     [Fact]
-    public void Id_IsCorrect_WithParameters()
+    public void Id_IsCorrect_WithSingleParameter()
     {
-        var param1 = Substitute.For<ParameterData>();
-        param1.Type.ShortName.Returns("String");
-        param1.Name.Returns()
+        var param = Substitute.For<ParameterInfo>();
+        param.ParameterType.Returns(typeof(string));
 
         var methodInfo = Substitute.For<MethodInfo>();
         methodInfo.Name.Returns("Execute");
         methodInfo.ReturnType.Returns(typeof(void));
-        methodInfo.GetParameters().Returns([]);
+        methodInfo.GetParameters().Returns([param]);
 
         ExecutableMemberData memberData = new MethodData(methodInfo);
 
-        memberData.Id.Should().Be("Execute");
+        memberData.Id.Should().Be("Execute(System.String)");
+    }
+
+    [Fact]
+    public void Id_IsCorrect_WithMultipleParameters()
+    {
+        string expectedId = "Execute(System.String,System.Type,System.Int32@)";
+
+        var param1 = Substitute.For<ParameterInfo>();
+        param1.ParameterType.Returns(typeof(string));
+
+        var param2 = Substitute.For<ParameterInfo>();
+        param2.ParameterType.Returns(typeof(Type));
+
+        var param3 = Substitute.For<ParameterInfo>();
+        param3.ParameterType.Returns(typeof(int).MakeByRefType());
+
+        var methodInfo = Substitute.For<MethodInfo>();
+        methodInfo.Name.Returns("Execute");
+        methodInfo.ReturnType.Returns(typeof(void));
+        methodInfo.GetParameters().Returns([param1, param2, param3]);
+
+        ExecutableMemberData memberData = new MethodData(methodInfo);
+
+        memberData.Id.Should().Be(expectedId);
     }
 }
