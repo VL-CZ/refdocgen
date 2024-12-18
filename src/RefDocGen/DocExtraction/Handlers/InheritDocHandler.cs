7b958456dd4ac6127d3aeae4549b3ade7acb1afd
Vojtěch Lengál <vojta.lengal@gmail.com>
2024-12-18 13:33:41 +0100

    DFS done?

--- src/RefDocGen/DocExtraction/Handlers/InheritDocHandler.cs
+++ src/RefDocGen/DocExtraction/Handlers/InheritDocHandler.cs
@@ -7,6 +7,8 @@ namespace RefDocGen.DocExtraction.Handlers;
 
 record MemberRecord(ObjectTypeData Type, string MemberId, XElement DocComment);
 
+record Node(ObjectTypeData Type, string MemberId);
+
 internal class InheritDocHandler
 {
     /// <summary>
@@ -32,39 +34,70 @@ internal class InheritDocHandler
 
     private void Handle(MemberRecord memberRecord)
     {
+        var node = new Node(memberRecord.Type, memberRecord.MemberId);
+        var docComment = Handle(node);
+
+        if (docComment is null)
+        {
+            return;
+        }
+
+        memberRecord.DocComment.RemoveNodes();
+        memberRecord.DocComment.Add(docComment.Nodes());
+
+        toReturn.Add(memberRecord.DocComment);
+    }
+
+    private XElement? Handle(Node node)
+    {
+        var parentTypes = GetParentTypes(node.Type);
+
+        foreach (var parentType in parentTypes)
+        {
+            var parentId = parentType.HasTypeParameters
+                ? $"{parentType.FullName}`{parentType.TypeParameters.Count}"
+                : parentType.Id;
+
+            if (node.Type.AllMembers.TryGetValue(node.MemberId, out var member)
+                && typeRegistry.ObjectTypes.TryGetValue(parentId, out var parent))
+            {
+                // desired member found
+                if (parent.AllMembers.TryGetValue(node.MemberId, out var parentMember))
+                {
+                    if (parentMember.RawDocComment is null)
+                    {
+                        var parentNode = new Node(parent, node.MemberId);
+                        var parentDocComment = Handle(parentNode);
+
+                        if (parentDocComment is not null)
+                        {
+                            return parentDocComment;
+                        }
+                    }
+                    else
+                    {
+                        return parentMember.RawDocComment;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private IEnumerable<ITypeNameData> GetParentTypes(ObjectTypeData type)
+    {
         var parentTypes = new List<ITypeNameData>();
 
-        var baseType = memberRecord.Type.BaseType;
+        var baseType = type.BaseType;
 
         if (baseType is not null)
         {
             parentTypes.Add(baseType);
         }
 
-        parentTypes.AddRange(memberRecord.Type.Interfaces);
+        parentTypes.AddRange(type.Interfaces);
 
-        foreach (var parentType in parentTypes)
-        {
-            var id = parentType.HasTypeParameters
-                ? $"{parentType.FullName}`{parentType.TypeParameters.Count}"
-                : parentType.Id;
-
-            if (memberRecord.Type.AllMembers.TryGetValue(memberRecord.MemberId, out var member)
-                && typeRegistry.ObjectTypes.TryGetValue(id, out var parent))
-            {
-                if (parent.AllMembers.TryGetValue(memberRecord.MemberId, out var parentMember))
-                {
-                    if (parentMember.RawDocComment is null)
-                    {
-                        continue;
-                    }
-
-                    memberRecord.DocComment.RemoveNodes();
-                    memberRecord.DocComment.Add(parentMember.RawDocComment.Nodes());
-
-                    toReturn.Add(memberRecord.DocComment);
-                }
-            }
-        }
+        return parentTypes;
     }
 }
