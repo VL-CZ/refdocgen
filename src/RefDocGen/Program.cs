0e0b17a2f2ba41fa66bf87ca5a6c651c895a7c72
Vojtěch Lengál <vojta.lengal@gmail.com>
2025-01-06 11:50:13 +0100

    [T-29] use Razor templates

--- src/RefDocGen/Program.cs
+++ src/RefDocGen/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using RefDocGen.TemplateGenerators.Default;
+
+#pragma warning disable IDE0005 // add the namespace containing the Razor templates
+using RefDocGen.TemplateGenerators.Default.Templates;
+#pragma warning restore IDE0005
 
 namespace RefDocGen;
 
@@ -10,17 +17,33 @@ public static class Program
     /// <summary>
     /// Main method, entry point of the RefDocGen app
     /// </summary>
-    public static void Main()
+    public static async Task Main()
     {
         string? rootPath = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent?.Parent?.Parent?.Parent?.Parent?.FullName;
         string dllPath = Path.Join(rootPath, "demo-lib", "MyLibrary.dll");
         string docPath = Path.Join(rootPath, "demo-lib", "MyLibrary.xml");
 
         string projectPath = Path.Join(rootPath, "src", "RefDocGen");
-        string templatePath = "TemplateGenerators/Default/Templates/Default";
-        string outputDir = Path.Combine(projectPath, "out");
+        //string templatePath = "TemplateGenerators/Default/Templates/Default";
+        //string outputDir = Path.Combine(projectPath, "out");
+        string outputDir = Path.Combine(projectPath, "out-razor");
 
-        var templateGenerator = new DefaultTemplateGenerator(projectPath, templatePath, outputDir);
+        IServiceCollection services = new ServiceCollection();
+        _ = services.AddLogging();
+
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+        var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+        await using var htmlRenderer = new HtmlRenderer(serviceProvider, loggerFactory);
+
+        //var templateGenerator = new DefaultTemplateGenerator(projectPath, templatePath, outputDir);
+        var templateGenerator = new RazorTemplateGenerator<
+                DelegateTypeTemplate,
+                EnumTypeTemplate,
+                NamespaceDetailTemplate,
+                NamespaceListTemplate,
+                ObjectTypeTemplate
+            >(htmlRenderer, outputDir);
 
         var docGenerator = new DocGenerator(dllPath, docPath, templateGenerator);
         docGenerator.GenerateDoc();
---
+++ src/RefDocGen/TemplateGenerators/Default/RazorTemplateGenerator.cs
@@ -0,0 +1,160 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using RefDocGen.CodeElements.Abstract;
+using RefDocGen.CodeElements.Abstract.Types;
+using RefDocGen.CodeElements.Abstract.Types.Delegate;
+using RefDocGen.CodeElements.Abstract.Types.Enum;
+using RefDocGen.TemplateGenerators.Default.TemplateModelCreators;
+using RefDocGen.TemplateGenerators.Default.TemplateModels.Namespaces;
+using RefDocGen.TemplateGenerators.Default.TemplateModels.Types;
+using RefDocGen.TemplateGenerators.Tools;
+
+namespace RefDocGen.TemplateGenerators.Default;
+
+/// <summary>
+/// Class used for generating Razor templates using the <see cref="ObjectTypeTM"/> as a type template model and <see cref="NamespaceTM"/> as a namespace template model.
+/// </summary>
+/// <typeparam name="TDelegateTemplate">Type of the Razor component representing the delegate type.</typeparam>
+/// <typeparam name="TEnumTemplate">Type of the Razor component representing the enum type.</typeparam>
+/// <typeparam name="TNamespaceDetailTemplate">Type of the Razor component representing the namespace detail.</typeparam>
+/// <typeparam name="TNamespaceListTemplate">Type of the Razor component representing the namespace list.</typeparam>
+/// <typeparam name="TObjectTypeTemplate">Type of the Razor component representing the object type.</typeparam>
+internal class RazorTemplateGenerator<
+        TDelegateTemplate,
+        TEnumTemplate,
+        TNamespaceDetailTemplate,
+        TNamespaceListTemplate,
+        TObjectTypeTemplate
+    > : ITemplateGenerator
+
+    where TDelegateTemplate : IComponent
+    where TEnumTemplate : IComponent
+    where TNamespaceDetailTemplate : IComponent
+    where TNamespaceListTemplate : IComponent
+    where TObjectTypeTemplate : IComponent
+{
+    /// <summary>
+    /// The directory, where the generated output will be stored.
+    /// </summary>
+    private readonly string outputDir;
+
+    /// <summary>
+    /// Rendered of the Razor components.
+    /// </summary>
+    private readonly HtmlRenderer htmlRenderer;
+
+
+    private HtmlCommentParser htmlCommentParser = new();
+
+    /// <summary>
+    /// Initialize a new instance of <see cref="RazorTemplateGenerator{TDelegateTemplate, TEnumTemplate, TNamespaceDetailTemplate, TNamespaceListTemplate, TObjectTypeTemplate}"/> class.
+    /// </summary>
+    /// <param name="htmlRenderer">Rendered of the Razor components.</param>
+    /// <param name="outputDir">The directory, where the generated output will be stored.</param>
+    internal RazorTemplateGenerator(HtmlRenderer htmlRenderer, string outputDir)
+    {
+        this.outputDir = outputDir;
+        this.htmlRenderer = htmlRenderer;
+    }
+
+    /// <inheritdoc/>
+    public void GenerateTemplates(ITypeRegistry typeRegistry)
+    {
+        htmlCommentParser = new HtmlCommentParser(typeRegistry);
+
+        GenerateObjectTypeTemplates(typeRegistry.ObjectTypes);
+        GenerateEnumTemplates(typeRegistry.Enums);
+        GenerateDelegateTemplates(typeRegistry.Delegates);
+        GenerateNamespaceTemplates(typeRegistry);
+    }
+
+    /// <summary>
+    /// Generate the templates representing the individual object types.
+    /// </summary>
+    /// <param name="types">The type data to be used in the templates.</param>
+    private void GenerateObjectTypeTemplates(IEnumerable<IObjectTypeData> types)
+    {
+        var typeTemplateModels = types.Select(t => ObjectTypeTMCreator.GetFrom(t, htmlCommentParser));
+        GenerateTemplates<TObjectTypeTemplate, ObjectTypeTM>(typeTemplateModels);
+    }
+
+    /// <summary>
+    /// Generate the templates representing the individual enum types.
+    /// </summary>
+    /// <param name="enums">The enum data to be used in the templates.</param>
+    private void GenerateEnumTemplates(IEnumerable<IEnumTypeData> enums)
+    {
+        var enumTMs = enums.Select(EnumTMCreator.GetFrom);
+        GenerateTemplates<TEnumTemplate, EnumTypeTM>(enumTMs);
+    }
+
+    /// <summary>
+    /// Generate the templates representing the individual delegate types.
+    /// </summary>
+    /// <param name="delegates">The delegate data to be used in the templates.</param>
+    private void GenerateDelegateTemplates(IEnumerable<IDelegateTypeData> delegates)
+    {
+        var delegateTMs = delegates.Select(DelegateTMCreator.GetFrom);
+        GenerateTemplates<TDelegateTemplate, DelegateTypeTM>(delegateTMs);
+    }
+
+    /// <summary>
+    /// Generate the templates for the namespaces (both namespace list and individual namespace details pages).
+    /// </summary>
+    /// <param name="types">The type data to be used in the templates.</param>
+    private void GenerateNamespaceTemplates(ITypeRegistry types)
+    {
+        var namespaceTMs = NamespaceListTMCreator.GetFrom(types);
+
+        // namespace list template
+        GenerateTemplate<TNamespaceListTemplate, IEnumerable<NamespaceTM>>(namespaceTMs, "index");
+
+        // namespace detail templates
+        GenerateTemplates<TNamespaceDetailTemplate, NamespaceTM>(namespaceTMs);
+    }
+
+    /// <summary>
+    /// Generate the given templates using the provided template models.
+    /// </summary>
+    /// <param name="templateModels">Template models used for the templates generation.</param>
+    /// <typeparam name="TTemplateModel">Type of the template model to be used in the template.</typeparam>
+    /// <typeparam name="TTemplate">Type of the Razor component representing the template to generate.</typeparam>
+    private void GenerateTemplates<TTemplate, TTemplateModel>(IEnumerable<TTemplateModel> templateModels)
+        where TTemplate : IComponent
+        where TTemplateModel : ITemplateModelWithId
+    {
+        foreach (var tm in templateModels)
+        {
+            GenerateTemplate<TTemplate, TTemplateModel>(tm, tm.Id);
+        }
+    }
+
+    /// <summary>
+    /// Generate the given template using the provided template model.
+    /// </summary>
+    /// <param name="templateModel">Template model used for the template generation.</param>
+    /// <param name="outputFile">Name of the output file containing the generated template populated with the <paramref name="templateModel"/> data.</param>
+    /// <typeparam name="TTemplateModel">Type of the template model to be used in the template.</typeparam>
+    /// <typeparam name="TTemplate">Type of the Razor component representing the template to generate.</typeparam>
+    private void GenerateTemplate<TTemplate, TTemplateModel>(TTemplateModel templateModel, string outputFile)
+        where TTemplate : IComponent
+    {
+        string outputFileName = Path.Join(outputDir, $"{outputFile}.html");
+
+        string html = htmlRenderer.Dispatcher.InvokeAsync(async () =>
+        {
+            var paramDictionary = new Dictionary<string, object?>()
+            {
+                ["Model"] = templateModel
+            };
+
+            var parameters = ParameterView.FromDictionary(paramDictionary);
+            var output = await htmlRenderer.RenderComponentAsync<TTemplate>(parameters);
+
+            return output.ToHtmlString();
+        }).Result;
+
+
+        File.WriteAllText(outputFileName, html);
+    }
+}
