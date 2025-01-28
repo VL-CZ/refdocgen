ca19dd4281fcc50b76ae3c4c2dd83b8ff6fd5267
Vojtěch Lengál <vojta.lengal@gmail.com>
2025-01-28 23:12:22 +0100

    fix3

--- src/RefDocGen/TemplateGenerators/Default/DefaultTemplateGenerator.cs
+++ src/RefDocGen/TemplateGenerators/Default/DefaultTemplateGenerator.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
-using RefDocGen.TemplateGenerators.Default.Templates;
 using RefDocGen.TemplateGenerators.Shared;
 using RefDocGen.TemplateGenerators.Shared.Tools.DocComments.Html;
+
+#pragma warning disable IDE0005 // add the namespace containing the Razor templates
+using RefDocGen.TemplateGenerators.Default.Templates;
+#pragma warning restore IDE0005
 
 namespace RefDocGen.TemplateGenerators.Default;
 
