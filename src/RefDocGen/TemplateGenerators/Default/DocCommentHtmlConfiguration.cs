3fc46f0520cdfa8c735b6d5b7c0b305d7513fd97
Vojtěch Lengál <vojta.lengal@gmail.com>
2025-05-05 14:47:08 +0200

    v2

--- src/RefDocGen/TemplateGenerators/Default/DocCommentHtmlConfiguration.cs
+++ src/RefDocGen/TemplateGenerators/Default/DocCommentHtmlConfiguration.cs
@@ -23,69 +23,70 @@ internal class DocCommentHtmlConfiguration : IDocCommentHtmlConfiguration
     public virtual XElement ListItemElement => new XElement("li").WithClass("refdocgen-list-item");
 
     /// <inheritdoc />
-    public virtual XElement InlineCodeElement => new("code");
+    public virtual XElement InlineCodeElement => new XElement("code").WithClass("refdocgen-inline-code");
 
     /// <inheritdoc />
     public virtual XElement CodeBlockElement => new("pre",
-                                                    new XElement("code")
+                                                    new XElement("code").WithClass("refdocgen-code-block")
                                                 );
 
     /// <inheritdoc />
-    public virtual XElement ExampleElement => new("div");
+    public virtual XElement ExampleElement => new XElement("div").WithClass("refdocgen-example");
 
     /// <inheritdoc />
-    public virtual XElement ParamRefElement => new("code");
+    public virtual XElement ParamRefElement => new XElement("code").WithClass("refdocgen-paramref");
 
     /// <inheritdoc />
-    public virtual XElement TypeParamRefElement => new("code");
+    public virtual XElement TypeParamRefElement => new XElement("code").WithClass("refdocgen-typeparamref");
 
     /// <inheritdoc />
-    public virtual XElement SeeCrefElement =>
-        new("a",
-            new XAttribute("class", "link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover")
-        );
+    public virtual XElement SeeCrefElement => new XElement("a").WithClass("refdocgen-see-cref");
+    //new("a",
+    //    new XAttribute("class", "link-offset-2 link-underline-opacity-50 link-underline-opacity-100-hover")
+    //);
 
     /// <inheritdoc />
-    public virtual XElement SeeHrefElement => SeeCrefElement;
+    public virtual XElement SeeHrefElement => new XElement("a").WithClass("refdocgen-see-href");
 
     /// <inheritdoc />
-    public virtual XElement SeeLangwordElement => new("code");
+    public virtual XElement SeeLangwordElement => new XElement("code").WithClass("refdocgen-see-langword");
 
     /// <inheritdoc />
-    public virtual XElement SeeCrefNotFoundElement => new("code");
+    public virtual XElement SeeCrefNotFoundElement => new XElement("code").WithClass("refdocgen-see-cref-not-found");
 
     /// <inheritdoc />
-    public virtual XElement SeeAlsoCrefElement => SeeCrefElement;
+    public virtual XElement SeeAlsoCrefElement => new XElement("a").WithClass("refdocgen-seealso-cref");
 
     /// <inheritdoc />
-    public virtual XElement SeeAlsoHrefElement => SeeCrefElement;
+    public virtual XElement SeeAlsoHrefElement => new XElement("a").WithClass("refdocgen-seealso-href");
 
     /// <inheritdoc />
-    public virtual XElement SeeAlsoCrefNotFoundElement => new("code");
+    public virtual XElement SeeAlsoCrefNotFoundElement => new XElement("code").WithClass("refdocgen-seealso-cref-not-found");
 
     /// <inheritdoc />
-    public XElement TermElement => new("b");
+    public virtual XElement ListHeaderElement => new XElement("thead").WithClass("refdocgen-table-header");
 
     /// <inheritdoc />
-    public XElement ListHeaderElement => new("thead");
+    public virtual XElement TableListElement => new XElement("table").WithClass("refdocgen-table");
+    //, new XAttribute("class", "table w-auto"));
 
     /// <inheritdoc />
-    public XElement TableListElement => new("table", new XAttribute("class", "table w-auto"));
+    public virtual XElement TableItemElement => new XElement("tr").WithClass("refdocgen-table-item");
 
     /// <inheritdoc />
-    public XElement TableItemElement => new("tr");
-
-    /// <inheritdoc />
-    public XElement ListTermElement => new("b");
+    public virtual XElement ListTermElement => new XElement("b").WithClass("refdocgen-list-term");
 
     /// <inheritdoc />
     public XElement ListDescriptionElement => new("span",
                                                     " ",
-                                                    new XElement("span"));
+                                                    new XElement("span").WithClass("refdocgen-list-description"));
 
     /// <inheritdoc />
-    public XElement TableTermElement => new("td", new XAttribute("class", "text-muted"), new XElement("b"));
+    public XElement TableTermElement => new XElement("td").WithClass("refdocgen-table-term");
+    //new XAttribute("class", "text-muted")
+    //new XAttribute("class", "text-muted")
 
     /// <inheritdoc />
-    public XElement TableDescriptionElement => new("td", new XAttribute("class", "text-muted"));
+    public XElement TableDescriptionElement => new XElement("td").WithClass("refdocgen-table-element");
+    //new XAttribute("class", "text-muted")
 }
