df05ee4b91df45c88bd8bf37250044d0d7c68ff9
Vojtěch Lengál <vojta.lengal@gmail.com>
2025-06-08 15:37:18 +0200

    cleanup

--- src/RefDocGen/TemplateProcessors/Default/Templates/Static/script.js
+++ src/RefDocGen/TemplateProcessors/Default/Templates/Static/script.js
@@ -1,7 +1,8 @@
 
-function fetchVersions() {
-    // Get the original <ul> and its <li> items
-
+/**
+ * Loads the version list from the 'version-list' element, and populates the 'version-selector' dropdown with their data.
+ */
+function loadVersions() {
     const versionListElement = document.getElementById('version-list');
 
     if (versionListElement) {
@@ -11,8 +12,8 @@ function fetchVersions() {
 
         const currentVersion = document.getElementById('current-version').innerText.trim();
 
-        // Create a new <ul> element
-        const newUl = document.getElementById('version-selector');
+        // Get the 'version-selector'
+        const versionSelector = document.getElementById('version-selector');
 
         // Loop through each version and create a new <li> with a <a> inside
         versionItems.forEach(item => {
@@ -27,7 +28,6 @@ function fetchVersions() {
             const newUrl = currentUrl.replace(currentVersion, versionText);
 
             newA.href = newUrl;
-
             newA.textContent = versionText;
 
             if (versionText === currentVersion) {
@@ -36,35 +36,43 @@ function fetchVersions() {
 
             // Append the <a> to the <li> and the <li> to the new <ul>
             newLi.appendChild(newA);
-            newUl.appendChild(newLi);
+            versionSelector.appendChild(newLi);
         });
     }
 }
 
+/**
+ * Gets the other theme that passed in the parameter.
+ */
+function getOtherTheme(theme) {
+    return theme === 'light' ? 'dark' : 'light';
+}
+
+/**
+ * Switches the theme from light to dark or vice versa.
+ */
 function switchTheme() {
-    if (isLocalFile) {
+    if (isLocalFile) { // set the 'theme' URL param, and redirect
         const url = new URL(window.location.href);
         const currentTheme = url.searchParams.get('theme');
-        const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+        const newTheme = getOtherTheme(currentTheme);
 
         url.searchParams.set('theme', newTheme);
         window.location.href = url.toString();
     }
-    else {
+    else { // set the 'data-bs-theme' attribute + store into local storage
         const htmlElement = document.documentElement;
-
         const currentTheme = htmlElement.getAttribute('data-bs-theme');
-        const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-        htmlElement.setAttribute('data-bs-theme', newTheme);
+        const newTheme = getOtherTheme(currentTheme);
 
+        htmlElement.setAttribute('data-bs-theme', newTheme);
         localStorage.setItem('refdocgen-theme', newTheme);
     }
 }
 
 /**
- * Sets the language visibility
- * @param {any} selectedLang the selected language identifier
- * @param {any} allLangs identifiers of all languages
+ * Sets the visibility of language specific elements.
+ * @param {any} selectedLang the selected language identifier.
  */
 function setLanguageVisibility(selectedLang) {
 
@@ -72,7 +80,7 @@ function setLanguageVisibility(selectedLang) {
     const allLangs = Array.from(document.getElementsByClassName('lang-option'))
         .map(option => option.value);
 
-    // invalid language value -> return
+    // invalid language value -> do nothing
     if (!allLangs.includes(selectedLang)) {
         return;
     }
@@ -80,41 +88,24 @@ function setLanguageVisibility(selectedLang) {
     if (isLocalFile) {
         const url = new URL(window.location.href);
 
-        if (url.searchParams.get('lang') !== selectedLang) {
+        if (url.searchParams.get('lang') !== selectedLang) { // add the 'lang' URL param, and redirect
             url.searchParams.set('lang', selectedLang);
             window.location.href = url.toString();
         }
     }
-    else {
-        // Store into localStorage
+    else { // set 'data-language' attribute + store into local storage
+        document.documentElement.setAttribute('data-language', selectedLang);
         localStorage.setItem('refdocgen-language', selectedLang);
     }
-
-    document.documentElement.setAttribute('refdocgen-language', selectedLang);
-
-    //// Hide all language-specific elements
-    //allLangs.forEach(lang => {
-    //    if (lang == selectedLang) {
-    //        // Show only selected language elements
-    //        const selectedLangElements = document.getElementsByClassName(selectedLang);
-    //        Array.from(selectedLangElements).forEach(el => {
-    //            el.classList.remove('not-visible');
-    //        });
-    //    }
-    //    else {
-
-    //        const elements = document.getElementsByClassName(lang);
-    //        Array.from(elements).forEach(el => {
-    //            el.classList.add('not-visible');
-    //        });
-    //    }
-    //});
 }
 
+/**
+ * Updates all relative links at the page to include the same URL parameters as this page has.
+ */
 function updateRelativeLinks() {
     const urlParams = new URLSearchParams(window.location.search);
     if (!urlParams.get('theme')) {
-        urlParams.set('theme', 'light');
+        urlParams.set('theme', 'dark');
     }
 
     if (!urlParams.get('lang')) {
@@ -143,34 +134,19 @@ function updateRelativeLinks() {
 
 function main() {
     const languageSelector = document.getElementById('language-selector');
-    let savedLang = null;
+    const savedLang = document.documentElement.getAttribute('data-language');
 
-    if (isLocalFile) {
-        const searchParams = new URLSearchParams(window.location.search);
-        savedLang = searchParams.get('lang');
-    }
-    else {
-        savedLang = localStorage.getItem('refdocgen-language');
-    }
-
+    // set the saved language
     if (savedLang) {
-        // Set dropdown to saved value
         languageSelector.value = savedLang;
-
-        // Update visibility
-        setLanguageVisibility(savedLang);
     }
 
-    // fetch versions
-    fetchVersions();
+    // load versions, and create version selector dropdown
+    loadVersions();
 
     // switch theme on click
     const themeSwitcher = document.getElementById('theme-switcher');
     themeSwitcher.addEventListener('click', switchTheme);
-
-    if (isLocalFile) {
-        updateRelativeLinks();
-    }
 
     // go to search page on search bar click
     const menuSearchBar = document.getElementById('menu-search-bar');
@@ -191,6 +167,11 @@ function main() {
         // set language visibility
         setLanguageVisibility(selectedLang);
     });
+
+    // for local files, update all relative links to use the same URL params
+    if (isLocalFile) {
+        updateRelativeLinks();
+    }
 }
 
 if (document.readyState === "loading") {
