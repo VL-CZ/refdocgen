2ebfb5da3020610979676c70a031a110d756e952
Vojtěch Lengál <vojta.lengal@gmail.com>
2025-06-20 12:42:00 +0200

    v1

--- src/RefDocGen/Program.cs
+++ src/RefDocGen/Program.cs
@@ -1,8 +1,10 @@
+using CommandLine;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using RefDocGen.AssemblyAnalysis;
 using RefDocGen.CodeElements;
+using RefDocGen.TemplateProcessors;
 using RefDocGen.TemplateProcessors.Default;
 using RefDocGen.TemplateProcessors.Shared.Languages;
 using RefDocGen.Tools.Exceptions;
@@ -12,6 +14,39 @@ using System.Globalization;
 
 namespace RefDocGen;
 
+internal class Options
+{
+    [Value(0, Required = true)]
+    public string Source { get; set; }
+
+    [Option('o', "output-dir", HelpText = "The directory where output files will be saved.", Default = "reference-docs")]
+    public string OutputDirectory { get; set; }
+
+    [Option('v', "verbose", HelpText = "Enable verbose mode.", Default = false)]
+    public bool Verbose { get; set; }
+
+    [Option('t', "template-processor", HelpText = "The template processor to use.", Default = "default")]
+    public string TemplateProcessor { get; set; }
+
+    [Option('s', "static-pages-dir", HelpText = "Path to the folder containing static pages.", Default = null)]
+    public string? StaticPagesDirectory { get; set; }
+
+    [Option("version", HelpText = "The version string to use.", Default = null)]
+    public string? Version { get; set; }
+
+    [Option("min-visibility", HelpText = "Minimum visibility (e.g., Public, Internal).", Default = AccessModifier.Family)]
+    public AccessModifier MinVisibility { get; set; }
+
+    [Option("inheritance-mode", HelpText = "Member inheritance mode.", Default = MemberInheritanceMode.NonObject)]
+    public MemberInheritanceMode InheritanceMode { get; set; }
+
+    [Option("assemblies-to-exclude", HelpText = "Assemblies to exclude from processing.")]
+    public IEnumerable<string> AssembliesToExclude { get; set; }
+
+    [Option("namespaces-to-exclude", HelpText = "Namespaces to exclude from processing.")]
+    public IEnumerable<string> NamespacesToExclude { get; set; }
+}
+
 /// <summary>
 /// Program class, containing main method
 /// </summary>
@@ -20,57 +55,53 @@ public static class Program
     /// <summary>
     /// Main method, entry point of the RefDocGen app
     /// </summary>
-    public static async Task Main()
+    public static void Main(string[] args)
     {
-        string? rootPath = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent?.Parent?.Parent?.Parent?.Parent?.FullName;
-        string[] dllPaths = [Path.Join(rootPath, "demo-lib", "MyLibrary.dll"), Path.Join(rootPath, "demo-lib", "MyApp.dll")];
-        string[] docPaths = [Path.Join(rootPath, "demo-lib", "MyLibrary.xml"), Path.Join(rootPath, "demo-lib", "MyApp.xml")];
+        _ = Parser.Default.ParseArguments<Options>(args)
+                    .MapResult(
+                        Do,
+                        async _ => await Task.FromResult(1)
+                    );
+    }
 
-        string projectPath = Path.Join(rootPath, "src", "RefDocGen");
-        string outputDir = Path.Combine(projectPath, "out");
-        string staticPagesDir = "C:\\Users\\vojta\\UK\\mgr-thesis\\refdocgen\\demo-lib\\pages";
-        string? version = null;
+    private static async Task Do(Options o)
+    {
+        string[] dllPaths = [o.Source];
+        string[] docPaths = dllPaths.Select(p => p.Replace(".dll", ".xml")).ToArray();
 
         var assemblyDataConfig = new AssemblyDataConfiguration(
-            MinVisibility: AccessModifier.Private,
-            MemberInheritanceMode: MemberInheritanceMode.NonObject,
-            AssembliesToExclude: ["MyApp"],
-            NamespacesToExclude: ["MyLibrary.Exclude", "MyLibrary.Tools.Exclude"]
+            MinVisibility: o.MinVisibility,
+            MemberInheritanceMode: o.InheritanceMode,
+            AssembliesToExclude: o.AssembliesToExclude,
+            NamespacesToExclude: o.NamespacesToExclude
             );
 
-
-        bool verbose = true;
-        string outputTemplate = verbose
-            ? "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}"
-            : "[{Level:u}] {Message:lj}{NewLine}";
-
-        var level = verbose ? LogEventLevel.Information : LogEventLevel.Warning;
-
-        var serilogLogger = new LoggerConfiguration()
-            .MinimumLevel.Is(level)
-            .WriteTo.Console(outputTemplate: outputTemplate, formatProvider: CultureInfo.InvariantCulture)
-            .CreateLogger();
+        var serilogLogger = GetSerilogLogger(o.Verbose);
 
         IServiceCollection services = new ServiceCollection();
         _ = services.AddLogging(builder => builder.AddSerilog(serilogLogger, dispose: true));
 
         IServiceProvider serviceProvider = services.BuildServiceProvider();
         var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+        var logger = loggerFactory.CreateLogger("RefDocGen");
 
         await using var htmlRenderer = new HtmlRenderer(serviceProvider, loggerFactory);
 
         ILanguageConfiguration[] availableLanguages = [
-            new CSharpLanguageConfiguration(),
-            new TodoLanguageConfiguration()
+          new CSharpLanguageConfiguration(),
+                        new TodoLanguageConfiguration()
         ];
 
-        var logger = loggerFactory.CreateLogger("RefDocGen");
+        Dictionary<string, ITemplateProcessor> templateProcessors = new()
+        {
+            ["default"] = new DefaultTemplateProcessor(htmlRenderer, availableLanguages, o.StaticPagesDirectory, o.Version)
+        };
 
         try
         {
-            var templateProcessor = new DefaultTemplateProcessor(htmlRenderer, availableLanguages, staticPagesDir, version);
+            var templateProcessor = templateProcessors[o.TemplateProcessor];
 
-            var docGenerator = new DocGenerator(dllPaths, docPaths, templateProcessor, assemblyDataConfig, outputDir, logger);
+            var docGenerator = new DocGenerator(dllPaths, docPaths, templateProcessor, assemblyDataConfig, o.OutputDirectory, logger);
             docGenerator.GenerateDoc();
             Console.WriteLine("Done...");
         }
@@ -86,5 +117,21 @@ public static class Program
             }
 
         }
+    }
+
+    private static Serilog.Core.Logger GetSerilogLogger(bool verbose)
+    {
+        string outputTemplate = verbose
+            ? "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}"
+            : "[{Level:u}] {Message:lj}{NewLine}";
+
+        var level = verbose
+            ? LogEventLevel.Information
+            : LogEventLevel.Warning;
+
+        return new LoggerConfiguration()
+            .MinimumLevel.Is(level)
+            .WriteTo.Console(outputTemplate: outputTemplate, formatProvider: CultureInfo.InvariantCulture)
+            .CreateLogger();
     }
 }
