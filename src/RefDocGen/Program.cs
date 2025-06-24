53051b765e34b7c2abc6058b3cc75f29ddc24c5c
Vojtěch Lengál <vojta.lengal@gmail.com>
2025-06-24 16:00:44 +0200

    Revert "msbuild locator partially working"
    This reverts commit 12b1b175719640d203505d6f351a1217cfc4da26.

--- src/RefDocGen/Program.cs
+++ src/RefDocGen/Program.cs
@@ -1,8 +1,6 @@
 using CommandLine;
 using CommandLine.Text;
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.Build.Evaluation;
-using Microsoft.Build.Locator;
 using Microsoft.CodeAnalysis.MSBuild;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -39,8 +37,6 @@ public static class Program
     /// </summary>
     public static async Task Main(string[] args)
     {
-        MSBuildLocator.RegisterDefaults();
-
         var parser = new Parser(with => with.HelpWriter = null);
         var parserResult = parser.ParseArguments<CommandLineConfiguration>(args);
 
@@ -59,31 +55,12 @@ public static class Program
     {
         var projectPath = config.Input;
 
-        var props = new Dictionary<string, string>
-        {
-            ["Configuration"] = "Debug",
-            ["Platform"] = "AnyCPU"
-        };
+        var workspace = MSBuildWorkspace.Create();
+        var project = await workspace.OpenProjectAsync(config.Input);
 
-        
-        var project = new Project(projectPath, props, null);
+        var path = project.CompilationOutputInfo.AssemblyPath;
 
-        var outputPathProp = project.GetPropertyValue("OutputPath");
-        var assemblyName = project.GetPropertyValue("AssemblyName");
-        var outputType = project.GetPropertyValue("OutputType");
-
-        // Determine extension
-        string extension = outputType switch
-        {
-            "Library" => ".dll",
-            "Exe" or "WinExe" => ".exe",
-            _ => ".dll" // fallback
-        };
-
-        var outputDir = Path.Combine(project.DirectoryPath, outputPathProp);
-        var outputAssembly = Path.Combine(outputDir, assemblyName + extension);
-
-        string[] dllPaths = [outputAssembly];
+        string[] dllPaths = [path];
         string[] docPaths = [.. dllPaths.Select(p => p.Replace(".dll", ".xml"))];
 
         var assemblyDataConfig = new AssemblyDataConfiguration(
