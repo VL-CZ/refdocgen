115ed1d0018085aa3391098d46a0a412ce452d82
Vojtěch Lengál <vojta.lengal@gmail.com>
2025-06-27 11:13:25 +0200

    libraries in iother langaguages added

---
+++ tests/RefDocGen.ExampleFSharpLibrary/Library.fs
@@ -0,0 +1,14 @@
+namespace RefDocGen.ExampleFSharpLibrary
+
+/// <summary>
+/// Performs mathematical operations.
+/// </summary>
+module FSharpMathTools =
+
+    /// <summary>
+    /// Adds two integers.
+    /// </summary>
+    /// <param name="x">The first integer.</param>
+    /// <param name="y">The second integer.</param>
+    /// <returns>The sum of the integers.</returns>
+    let Add x y = x + y
---
+++ tests/RefDocGen.ExampleVbLibrary/Library.vb
@@ -0,0 +1,16 @@
+''' <summary>
+''' Performs mathematical operations.
+''' </summary>
+Public Module VbMathTools
+
+    ''' <summary>
+    ''' Adds two integers.
+    ''' </summary>
+    ''' <param name="x">The first integer.</param>
+    ''' <param name="y">The second integer.</param>
+    ''' <returns>The sum <paramref name="x"/> and <paramref name="y"/>.</returns>
+    Public Function Add(x As Integer, y As Integer) As Integer
+        Return x + y
+    End Function
+
+End Module
