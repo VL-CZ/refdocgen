795c830bafea8f1e043b78c14f0fa90231806437
Vojtěch Lengál <vojta.lengal@gmail.com>
2025-07-10 16:11:53 +0200

    [T-200] Add README & license

--- copy-dll.ps1
+++
@@ -1,5 +0,0 @@
-cp ..\sample-library\MyLibrary\bin\Debug\net8.0\* .\demo-lib\
-cp ..\sample-library\MyApp\bin\Debug\net8.0\* .\demo-lib\
-
-cp ..\sample-library\MyLibrary\bin\Debug\net8.0\MyLibrary.dll .\tests\RefDocGen.IntegrationTests\data\
-cp ..\sample-library\MyLibrary\bin\Debug\net8.0\MyLibrary.xml .\tests\RefDocGen.IntegrationTests\data\
