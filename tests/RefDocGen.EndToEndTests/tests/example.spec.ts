3fc752544f10c69c8e6654bf5b1314ed55f838dd
Vojtěch Lengál <vojta.lengal@gmail.com>
2025-10-31 21:21:14 +0100

    WIP

--- tests/RefDocGen.EndToEndTests/tests/example.spec.ts
+++ tests/RefDocGen.EndToEndTests/tests/example.spec.ts
@@ -16,3 +16,40 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'assembly RefDocGen.ExampleLibrary' })).toBeVisible();
 });
+
+test('test', async ({ page }) => {
+  await page.goto('https://vl-cz.github.io/refdocgen-demo-example-library/api/index.html');
+  await expect(page.locator('body')).toMatchAriaSnapshot(`- heading "RefDocGen API" [level=1]`);
+  await page.getByRole('link', { name: 'assembly RefDocGen.ExampleLibrary' }).click();
+  await expect(page.locator('body')).toMatchAriaSnapshot(`
+    - heading "assembly RefDocGen.ExampleLibrary" [level=1]
+    - button:
+      - img
+    `);
+  await page.getByRole('link', { name: 'namespace RefDocGen.ExampleLibrary', exact: true }).click();
+  await expect(page.locator('h1')).toMatchAriaSnapshot(`- heading "namespace RefDocGen.ExampleLibrary" [level=1]`);
+  await page.getByRole('link', { name: 'class User' }).click();
+  await expect(page.locator('body')).toMatchAriaSnapshot(`
+    - heading "public class User" [level=1]
+    - button:
+      - img
+    `);
+  await page.getByRole('link', { name: 'API' }).click();
+  await expect(page.locator('body')).toMatchAriaSnapshot(`
+    - heading "RefDocGen API" [level=1]
+    - button:
+      - img
+    `);
+});
+
+test('test', async ({ page }) => {
+  await page.goto('https://vl-cz.github.io/refdocgen-demo-example-library/api/index.html');
+  await page.getByRole('searchbox', { name: 'Search' }).click();
+  await page.getByRole('textbox', { name: 'Search input' }).fill('user');
+  await page.getByRole('link', { name: 'RefDocGen.ExampleLibrary.User class' }).click();
+  await expect(page.locator('body')).toMatchAriaSnapshot(`
+    - heading "public class User" [level=1]
+    - button:
+      - img
+    `);
+});
